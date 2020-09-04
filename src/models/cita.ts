2b6322a80e9764ba0f106a911414f1953f13703d
jaime <141670@unsaac.edu.pe>
2020-09-03 23:05:00 -0500

    agregando comandos

---
+++ src/models/cita.ts
@@ -0,0 +1,46 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, OneToMany } from "typeorm";
+import { Mascota } from "./mascota";
+import { Venta } from "./venta";
+import { Visita } from "./visita";
+
+@Entity()
+export class Cita {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({type: "varchar",length: 40})
+    Tipo: string;
+
+    @Column({type: "varchar",length: 40})
+    Apellidos: string;
+
+    @Column({type: "varchar",length: 20})
+    Email: string;
+
+    @Column({type: "varchar",length: 9})
+    Celular: string;
+
+    @Column({type: "varchar",length: 40})
+    Direccion: string;
+
+    @Column()
+    @CreateDateColumn()
+    CreatedAt: Date;
+
+    @Column()
+    @UpdateDateColumn()
+    UpdatedAt: Date;
+
+    @Column({type: "int"})
+    Estado: number
+
+    @OneToMany(() => Mascota, mascota => mascota.cliente)
+    mascotas: Mascota[]
+
+    @OneToMany(() => Venta, venta => venta.cliente)
+    ventas: Venta[]
+
+    @OneToMany(() => Visita, visita => visita.cliente)
+    visitas: Visita[]
+}
